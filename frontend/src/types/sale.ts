e205f0518cd5c4ce7dbbfb67fd0752648b6a65be
Marlon Santos <xsantos.marlonx@gmail.com>
2021-05-08 13:26:25 -0300

    BarChart integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,4 +1,10 @@
 export type SaleSum = {
-    sellerName: string,
-    sum: 220426.0
+    sellerName: string;
+    sum: 220426.0;
+}
+
+export type SaleSuccess = {
+    sellerName: string;
+    visited: number;
+    deals: number;
 }
